5f559a16a050fdc2a6136674d259a3636abcf9f7
Florian <mrminecrafttv19@gmail.com>
2025-03-31 08:14:12 +0200

    feat: add teleport command

---
+++ src/main/java/ovh/fedox/flockapi/command/TeleportCommand.java
@@ -0,0 +1,71 @@
+package ovh.fedox.flockapi.command;
+
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.mineacademy.fo.annotation.AutoRegister;
+import org.mineacademy.fo.command.SimpleCommand;
+import ovh.fedox.flockapi.util.SoundUtil;
+
+/**
+ * TeleportCommand.java - Teleport to a other player or coordinate
+ * <p>
+ * Created on 3/31/2025 at 8:05 AM by Fedox.
+ * Copyright © 2025 Fedox. All rights reserved.
+ */
+
+@AutoRegister
+public final class TeleportCommand extends SimpleCommand {
+
+	public TeleportCommand() {
+		super("teleport|tp");
+
+		setMinArguments(1);
+		setUsage("<player> [player] | <x> <y> <z>");
+	}
+
+	@Override
+	protected void onCommand() {
+		checkConsole();
+
+		if (args.length == 1) {
+			final Player target = findPlayer(args[0]);
+
+			if (target == null) {
+				tellError("Spieler nicht gefunden.");
+				SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.FAILURE);
+				return;
+			}
+
+			getPlayer().teleport(target);
+			tellSuccess("Du wurdest zu '" + target.getName() + "' teleportiert.");
+			SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.SUCCESS);
+		} else if (args.length == 2) {
+			final Player player = findPlayer(args[0]);
+			final Player target = findPlayer(args[1]);
+
+			if (player == null || target == null) {
+				tellError("Spieler nicht gefunden.");
+				SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.FAILURE);
+				return;
+			}
+
+			player.teleport(target);
+			tellSuccess("Du hast '" + player.getName() + "' zu '" + target.getName() + "' teleportiert.");
+			SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.SUCCESS);
+		} else if (args.length == 3) {
+			try {
+				final double x = Double.parseDouble(args[0]);
+				final double y = Double.parseDouble(args[1]);
+				final double z = Double.parseDouble(args[2]);
+
+				getPlayer().teleport(new Location(getPlayer().getWorld(), x, y, z));
+				tellSuccess("Du wurdest zu '" + x + " " + y + " " + z + "' teleportiert.");
+				SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.SUCCESS);
+			} catch (NumberFormatException e) {
+				tellError("Ungültige Koordinaten.");
+				SoundUtil.playSound(getPlayer(), SoundUtil.SoundType.FAILURE);
+			}
+		}
+	}
+}
