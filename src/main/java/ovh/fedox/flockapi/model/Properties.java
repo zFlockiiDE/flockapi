b120ccec4452c2224eddc8bc4adb284073edea6a
Florian <mrminecrafttv19@gmail.com>
2025-04-01 14:09:34 +0200

    feat: add Properties class

---
+++ src/main/java/ovh/fedox/flockapi/model/Properties.java
@@ -0,0 +1,54 @@
+package ovh.fedox.flockapi.model;
+
+
+import org.mineacademy.fo.Common;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Properties.java - The properties utility stuff
+ * <p>
+ * Created on 4/1/2025 at 2:03 PM by Fedox.
+ * Copyright © 2025 Fedox. All rights reserved.
+ */
+
+public class Properties {
+
+	private final java.util.Properties props = new java.util.Properties();
+
+	/**
+	 * Create a new properties object
+	 */
+	public Properties() {
+		try {
+			props.load(Files.newInputStream(Paths.get("server.properties")));
+		} catch (IOException e) {
+			Common.logFramed("Failed to load properties file: ", e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Get a property
+	 *
+	 * @param key the key
+	 * @return the property
+	 */
+	public String getProperty(String key) {
+		return props.getProperty(key);
+	}
+
+	/**
+	 * Set a property
+	 *
+	 * @param key   the key
+	 * @param value the value
+	 */
+	public void setProperty(String key, String value) {
+		props.setProperty(key, value);
+	}
+
+
+}
